8f49e846972e924556f8c192089a13b6bca3b095
dhairyaameria <dhairya.a@somaiya.edu>
2023-04-15 21:22:51 +0530

    error fix

--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -1,11 +1,8 @@
 import React from "react";
 
 // components
-import HeaderStats from "components/Headers/HeaderStats.js";
 import CardLineChart from "components/Cards/CardLineChart.js";
 import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 import CardProfile from "components/Cards/CardProfile.js";
 
 export default function Dashboard() {
@@ -22,15 +19,6 @@ export default function Dashboard() {
             <CardBarChart />
           </div>
         </div>
-        <div className="flex flex-wrap mt-4">
-          <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-            <CardPageVisits />
-          </div>
-          <div className="w-full xl:w-4/12 px-4">
-            <CardSocialTraffic />
-          </div>
-        </div>
-
         <div className="w-4/12 lg:w-4/12 px-4">
           <CardProfile />
         </div>
